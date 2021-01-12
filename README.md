6b0f63a102b80076f474f983d987a7c780acf8be
bumblebee211196 <bumblebee211196@gmail.com>
2021-01-12 19:46:07 +0530

    :tada: Add solution to problem 3

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 A repository containing solutions to problems from [DailyCodingProblem](https://www.dailycodingproblem.com/).
 
 1. [Problem #1](dcp_1/)
-1. [Problem #2](dcp_2/)
+2. [Problem #2](dcp_2/)
+3. [Problem #3](dcp_3/)
---
+++ dcp_3/solution.py
@@ -0,0 +1,36 @@
+import json
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def serialize(root):
+    node_serialized = {}
+    if root:
+        node_serialized['val'] = root.val
+        if root.left:
+            node_serialized['left'] = serialize(root.left)
+        if root.right:
+            node_serialized['right'] = serialize(root.right)
+    return json.dumps(node_serialized)
+
+
+
+def deserialize(root_serialized):
+    node_serialized = json.loads(root_serialized)
+    root = None
+    if node_serialized['val']:
+        root = Node(node_serialized['val'])
+        if 'left' in node_serialized:
+            root.left = deserialize(node_serialized['left'])
+        if 'right' in node_serialized:
+            root.right = deserialize(node_serialized['right'])
+    return root
+
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
