cf31cf01323afc0622b6bc9dea320aa6d769d4a0
bumblebee211196 <bumblebee211196@gmail.com>
2021-01-12 23:56:24 +0530

    :tada: Add solution to problem 7

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ A repository containing solutions to problems from [DailyCodingProblem](https://
 4. [Problem #4](dcp_4/)
 5. [Problem #5](dcp_5/)
 6. [Problem #6](dcp_6/)
+6. [Problem #7](dcp_7/)
---
+++ dcp_7/solution.py
@@ -0,0 +1,18 @@
+def solution(code_str):
+    if len(code_str) == 1:
+        return 1
+    n = len(code_str)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1 if code_str[0] != '0' else 0
+    for i in range(1, n):
+        if code_str[i] != '0':
+            dp[i + 1] += dp[i]
+        if code_str[i - 1] != '0' and 10 <= int(code_str[i - 1:i + 1]) <= 26:
+            dp[i + 1] += dp[i - 1]
+    return dp[n]
+
+
+assert solution('111') == 3
+assert solution('12345') == 3
+assert solution('11111') == 8
