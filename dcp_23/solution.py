0c19f880b3b6229fcf546d56550eeedb3b2ef925
bumblebee211196 <bumblebee211196@gmail.com>
2021-01-18 23:12:30 +0530

    :sparkle: Add A* approach

--- dcp_23/solution.py
+++ dcp_23/solution.py
@@ -1,22 +1,25 @@
-from collections import deque
+from heapq import heappop, heappush
+
+DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 def solution(mat, src, dst):
     m, n = len(mat), len(mat[0])
     x, y = src
-    dq = deque([(x, y, 0)])
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    if mat[x][y] == 't':
+        return -1
+    h = [(0, 0, x, y)]
     visited = set()
-    while dq:
-        x, y, dist = dq.popleft()
+    heuristic = lambda i, j: max(m - i - 1, n - j - 1)
+    while h:
+        heuristic_val, dist, x, y = heappop(h)
         if (x, y) == dst:
             return dist
-        for x_, y_ in directions:
+        for x_, y_ in DIRECTIONS:
             nx, ny = x + x_, y + y_
             if 0 <= nx < m and 0 <= ny < n and mat[nx][ny] == 'f' and (nx, ny) not in visited:
                 visited.add((nx, ny))
-                dq.append((nx, ny, dist + 1))
-    return -1
+                heappush(h, (dist + 1 + heuristic(nx, ny), dist + 1, nx, ny))
 
 
 matrix = [['f', 'f', 'f', 'f'],
@@ -24,7 +27,7 @@ matrix = [['f', 'f', 'f', 'f'],
           ['f', 'f', 'f', 'f'],
           ['f', 'f', 'f', 'f']]
 assert solution(matrix, (0, 0), (0, 0)) == 0
-assert solution(matrix, (1, 0), (0, 0)) == 1
+assert solution(matrix, (1, 0), (0, 0)) == -1
 assert solution(matrix, (3, 0), (0, 0)) == 7
 assert solution(matrix, (3, 0), (0, 3)) == 6
 
@@ -33,7 +36,7 @@ matrix = [['f', 'f', 'f', 'f'],
           ['f', 'f', 'f', 'f'],
           ['f', 'f', 'f', 'f']]
 assert solution(matrix, (0, 0), (0, 0)) == 0
-assert solution(matrix, (1, 0), (0, 0)) == 1
+assert solution(matrix, (1, 0), (0, 0)) == -1
 assert solution(matrix, (3, 0), (0, 0)) == 9
 assert solution(matrix, (3, 0), (0, 3)) == 6
 assert solution(matrix, (2, 0), (3, 3)) == 4
