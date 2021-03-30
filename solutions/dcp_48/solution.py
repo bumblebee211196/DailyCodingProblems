d94ee2772d1ba5a9371ef39e529aff88e05e62b8
S Sathish Babu <sathish.babu@zohocorp.com>
2021-03-30 16:16:01 +0530

    :tada: Add solution to problem 48

---
+++ solutions/dcp_48/solution.py
@@ -0,0 +1,35 @@
+class Tree:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def solution(preorder, inorder):
+    if not preorder:
+        return
+    root = Tree(preorder[0])
+    root_idx = inorder.index(root.val)
+    root.left = solution(preorder[1: 1 + root_idx], inorder[:root_idx])
+    root.right = solution(preorder[1 + root_idx:], inorder[1 + root_idx:])
+    return root
+
+
+tree = solution(preorder=['a', 'b', 'd', 'e', 'c', 'f', 'g'],
+                inorder=['d', 'b', 'e', 'a', 'f', 'c', 'g'])
+assert tree.val == 'a'
+assert tree.left.val == 'b'
+assert tree.left.left.val == 'd'
+assert tree.left.right.val == 'e'
+assert tree.right.val == 'c'
+assert tree.right.left.val == 'f'
+assert tree.right.right.val == 'g'
+
+tree = solution(preorder=['a', 'b', 'd', 'e', 'c', 'g'],
+                inorder=['d', 'b', 'e', 'a', 'c', 'g'])
+assert tree.val == 'a'
+assert tree.left.val == 'b'
+assert tree.left.left.val == 'd'
+assert tree.left.right.val == 'e'
+assert tree.right.val == 'c'
+assert tree.right.right.val == 'g'
