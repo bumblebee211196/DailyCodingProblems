0e21585dd1d5774c66973a421c00f66def9047f7
S Sathish Babu <sathish.babu@zohocorp.com>
2021-10-18 19:30:23 +0530

    :sparkles: Add solution and testcases to problem 61

---
+++ solutions/dcp_61/solution.py
@@ -0,0 +1,12 @@
+def solution(x, y):
+    if y == 1:
+        return x
+    res = solution(x, y // 2)
+    if y % 2 == 0:
+        return res * res
+    return res * res * x
+
+
+assert solution(2, 10) == 1024
+assert solution(2, 5) == 32
+    
