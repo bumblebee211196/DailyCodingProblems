aeb508832f2e0637a48bd8d16f82428f1bf88b37
S Sathish Babu <sathish.babu@zohocorp.com>
2021-11-25 19:46:23 +0530

    ✏️ Typos

--- tests/test_dcp1.py
+++ tests/test_dcp1.py
@@ -1,23 +1,17 @@
-"""
-test_dcp1.py: Description of what test_dcp1.py does.
-"""
-
-__author__ = "S Sathish Babu"
-__date__ = "28/05/21 Friday 8:56 PM"
-__email__ = "sathish.babu@zohocorp.com"
-
 import pytest
 
 from solutions.dcp_1.solution import solution
 
 
 class TestSolution:
-
-    @pytest.mark.parametrize("arr, k, output", [
-        ([10, 15, 3, 7, 19, 12, 1, 8], 27, True),
-        ([10, 15, 3, 7, 19, 12, 1, 8], 50, False),
-        ([], 10, False),
-        ([1], 10, False),
-    ])
+    @pytest.mark.parametrize(
+        "arr, k, output",
+        [
+            ([10, 15, 3, 7, 19, 12, 1, 8], 27, True),
+            ([10, 15, 3, 7, 19, 12, 1, 8], 50, False),
+            ([], 10, False),
+            ([1], 10, False),
+        ],
+    )
     def test_solution(self, arr, k, output):
         assert solution(arr, k) == output
--- tests/test_dcp2.py
+++ tests/test_dcp2.py
@@ -1,23 +1,17 @@
-"""
-test_dcp2.py: Description of what test_dcp2.py does.
-"""
-
-__author__ = "S Sathish Babu"
-__date__ = "28/05/21 Friday 9:55 PM"
-__email__ = "sathish.babu@zohocorp.com"
-
 import pytest
 
 from solutions.dcp_2.solution import solution
 
 
 class TestSolution:
-
-    @pytest.mark.parametrize("arr, output", [
-        ([1, 2, 3, 4, 5], [120, 60, 40, 30, 24]),
-        ([1, 2, 0, 3, 5], [0, 0, 30, 0, 0]),
-        ([3, 2, 1], [2, 3, 6]),
-        ([], []),
-    ])
+    @pytest.mark.parametrize(
+        "arr, output",
+        [
+            ([1, 2, 3, 4, 5], [120, 60, 40, 30, 24]),
+            ([1, 2, 0, 3, 5], [0, 0, 30, 0, 0]),
+            ([3, 2, 1], [2, 3, 6]),
+            ([], []),
+        ],
+    )
     def test_solution(self, arr, output):
         assert solution(arr) == output
