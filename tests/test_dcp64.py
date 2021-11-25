802c6ac8d5f082b38ac5da62f2ff7f53ee035933
S Sathish Babu <sathish.babu@zohocorp.com>
2021-11-25 19:50:21 +0530

    ✅ Remove long running test and fix test output for input 1

--- tests/test_dcp64.py
+++ tests/test_dcp64.py
@@ -7,11 +7,11 @@ class TestSolution:
     @pytest.mark.parametrize(
         "n, output",
         [
-            (1, 0),
+            (1, 1),
             (2, 0),
             (3, 0),
             (4, 0),
-            (5, 1728),
+            # (5, 1728), Takes more than a minute or two
         ],
     )
     def test_solution(self, n, output):
